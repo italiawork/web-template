b8f70ded5e806dca07c7d138bf643cc483b1c544
qudwill <qudwill@gmail.com>
2024-12-06 17:58:12 +0400

    feat: Listing_Checkout dataLayer event

--- src/containers/ListingPage/ListingPage.shared.js
+++ src/containers/ListingPage/ListingPage.shared.js
@@ -450,6 +450,13 @@ export const handleCustomSubmit = parameters => values => {
   // Clear previous Stripe errors from store if there is any
   onInitializeCardPaymentData();
 
+  pushDataLayerEvent({
+    dataLayer: {
+      email: currentUser.attributes.email,
+    },
+    dataLayerName: 'Listing_Checkout',
+  });
+
   // Redirect to CheckoutPage
   history.push(
     createResourceLocatorString(
